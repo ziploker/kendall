beebb34cfa048e87624eb005717fe03b0bb3157c
amsterdamAL <amsterdamAL@gmail.com>
2020-02-21 14:11:17 -0500

    first commit fix

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
+  #get 'welcome/index'
+  #get 'hello_world', to: 'hello_world#index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  root to: 'welcome#index'
 end
