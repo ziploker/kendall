7df80dfe247b56323b92d491db24059208c5aa34
amsterdamAL <amsterdamAL@gmail.com>
2020-02-22 16:39:46 -0500

    added gsap

--- app/javascript/packs/components/Topbar.jsx
+++ app/javascript/packs/components/Topbar.jsx
@@ -1,5 +1,9 @@
-import React from 'react'
+import React, {useEffect, useState} from 'react'
 import styled from 'styled-components'
+import logoPng from "../../../assets/images/logo.png";
+//import welcome from "../../../assets/stylesheets/welcome.scss"
+import { gsap } from 'gsap';
+
 
 
 
@@ -7,49 +11,107 @@ const Wrapper = styled.header`
 
     background: pink;
     display: flex;
+    justify-content: flex-end;
+    align-items: center;
+    padding: 30px 7%;
+    
+
+    
+    
     
 
 `;
 
-const H1 = style.h1`
+const Img = styled.img`
+    width: 160px;
+    cursor: pointer;
+    margin-right: auto;
+    transform: translate(30px,-30px);
+    opacity: 0;
 
-   
-
-`;
-
-const Nav = style.nav`
-
-
-`;
-
-const Ul = style.ul`
+    
 
 
-`;
-
-const Li = style.li`
+    
 
 
 `;
 
-const A = style.a`
 
+const Nav = styled.nav`
+
+    transform: translate(-30px,-30px);
+    opacity: 0;
+    
+    ul{
+        
+        list-style: none;
+
+        li, a{
+            
+            font-family: 'Nunito', sans-serif;
+            font-weight: 500;
+            font-size: 16px;
+            
+            color: black;
+            
+            text-decoration: none;
+        
+        
+        }
+    
+        li{
+            display: inline-block;
+            padding: 0px 20px;
+
+        
+            a{
+                transition: all 0.3s ease 0s;
+
+                &:hover{
+
+                    color: gray;
+
+                }
+            
+            }
+        
+        }
+    
+    
+    
+    }
+    
+    
 
 `;
 
-function Topbar() {
+function Topbar(props){
+    
+    const logo = React.createRef();
+    const navbar = React.createRef();
+    
+    useEffect( () => {
+
+        gsap.to(logo.current, {x: "0px", y: "0px", opacity: 1, duration: 1});
+        gsap.to(navbar.current, {x: "0px", y: "0px", opacity: 1, duration: 1});
+
+    },[]);
+    
+    
     return (
         <Wrapper>
-            <H1>LOGO!!</H1>
+            
+            <Img ref={logo} src={logoPng} alt="company logo" />
 
-            <Nav>
-                <Ul>
-                    <Li><A href="#">Link1</A></Li>
-                    <Li><A href="#">Link2</A></Li>
-                    <Li><A href="#">Link3</A></Li>
-                    <Li><A href="#">Link4</A></Li>
+            <Nav ref={navbar}>
+                <ul>
+                    <li><a href="#">Link1</a></li>
+                    <li><a href="#">Link2</a></li>
+                    <li><a href="#">Link3</a></li>
+                    <li><a href="#">Link4</a></li>
 
-                </Ul>
+                </ul>
 
             </Nav>
 
