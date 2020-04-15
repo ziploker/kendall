7e50ebc08ccbc2c421e4679d7fd449f718b2d98c
amsterdamAL <amsterdamAL@gmail.com>
2020-04-15 02:04:44 -0400

    added leads form

--- app/javascript/packs/components/LowerSection.jsx
+++ app/javascript/packs/components/LowerSection.jsx
@@ -12,7 +12,7 @@ const Section = styled.section`
     //background: rgb(136,189,188);
     //background: radial-gradient(circle, rgba(136,189,188,1) 0%, rgba(158,190,189,0.9612044646960347) 41%);
     background: #F7C562;
-    height: 100vh;
+    //height: 100vh;
     min-height: 400px;
     position: relative;
 
@@ -20,12 +20,31 @@ const Section = styled.section`
 `;
 var createReactClass = require('create-react-class');
 
+
+
+const Form = styled.form`
+
+  display: grid;
+  //grid-template-columns: 90%;
+  grid-gap: 1.5rem;
+  
+
+
+`;
+
+const FormWrapper = styled.div`
+
+  display: flex;
+  justify-content: center;
+  padding: 20px;
+`;
+
 var NewForm = createReactClass({
     
     
     getInitialState: function() {
       return {
-        name: 'BOB',
+        name: '',
         phone: '',
         email: '',
         company: '',
@@ -75,63 +94,82 @@ var NewForm = createReactClass({
     },
     render: function() {
       return(
-        <form className="form-inline" onSubmit={this.handleAdd}>
-          <div className="form-group">
-            <input type="text"
-                   className="form-control"
-                   name="name"
-                   placeholder="Name"
-                   ref="name"
-                   value={this.state.name}
-                   onChange={this.handleChange} />
-          </div>
-          <div className="form-group">
-            <input type="text"
-                   className="form-control"
-                   name="phone"
-                   placeholder="Phone"
-                   ref="phone"
-                   value={this.state.phone}
-                   onChange={this.handleChange} />
-          </div>
-          <div className="form-group">
-            <input type="text"
-                   className="form-control"
-                   name="email"
-                   placeholder="email"
-                   ref="email"
-                   value={this.state.email}
-                   onChange={this.handleChange} />  
-          </div>
-          <div className="form-group">
-            <input type="text"
-                   className="form-control"
-                   name="company"
-                   placeholder="company"
-                   ref="company"
-                   value={this.state.company}
-                   onChange={this.handleChange} />
-          </div>
-          <div className="form-group">
-            <input type="text"
-                   className="form-control"
-                   name="zip"
-                   placeholder="zip code"
-                   ref="zip"
-                   value={this.state.zip}
-                   onChange={this.handleChange} />
-          </div>
-          <div className="form-group">
-            <input type="text"
-                   className="form-control"
-                   name="message"
-                   placeholder="message"
-                   ref="message"
-                   value={this.state.message}
-                   onChange={this.handleChange} />
-          </div>
-          <button type="submit" className="btn btn-primary">Add</button>
-        </form>
+
+        <FormWrapper>
+          <Form className="form-inline" onSubmit={this.handleAdd}>
+            
+            <div className="form-group">
+            <label>Name</label><br/>
+              <input type="text"
+                    className="form-control"
+                    name="name"
+                    placeholder=""
+                    ref="name"
+                    value={this.state.name}
+                    onChange={this.handleChange} />
+            </div>
+            
+            
+            <div className="form-group">
+            <label>Phone Number</label><br/>
+              <input type="text"
+                    className="form-control"
+                    name="phone"
+                    placeholder="(305) 555-3210"
+                    ref="phone"
+                    value={this.state.phone}
+                    onChange={this.handleChange} />
+            </div>
+            
+            
+            <div className="form-group">
+            <label>Email</label><br/>
+              <input type="text"
+                    className="form-control"
+                    name="email"
+                    placeholder=""
+                    ref="email"
+                    value={this.state.email}
+                    onChange={this.handleChange} />  
+            </div>
+            
+            
+            <div className="form-group">
+            <label>What type of service do you need?</label><br/>
+              <select value={this.state.value} onChange={this.handleChange}>
+                <option value="home/personal">Home/Personal</option>
+                <option value="business">Business</option>
+                <option value="other">Other</option>
+                
+              </select>
+            </div>
+            
+            
+            <div className="form-group">
+            <label>Zip Code</label><br/>
+              <input type="text"
+                    className="form-control"
+                    name="zip"
+                    placeholder=""
+                    ref="zip"
+                    value={this.state.zip}
+                    onChange={this.handleChange} />
+            </div>
+            
+          
+            <div className="form-group">
+              <label>Message</label><br/>
+              <input type="text"
+                    className="form-control"
+                    name="message"
+                    placeholder=""
+                    ref="message"
+                    value={this.state.message}
+                    onChange={this.handleChange} />
+            </div>
+            <button type="submit" className="btn btn-primary">Add</button>
+          </Form>
+        </FormWrapper>
       )
     }
   });
